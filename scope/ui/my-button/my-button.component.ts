2807449b00978fa5da0c91d99db140d3d385ae63
Andrei <fateeand@gmail.com>
2023-01-15 00:33:40 +0200

    first commit

---
+++ scope/ui/my-button/my-button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-button',
+  template: `
+    <button>
+    my-button works!
+    </button>
+`,
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent {
+  constructor() { }
+}
